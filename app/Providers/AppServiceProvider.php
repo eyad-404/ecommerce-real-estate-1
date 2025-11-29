4a3cd87fab2598635bd2a685522fd5461ac4a7c5
pola-raouf <pola2302440@miuegypt.edu.eg>
2025-11-29 13:37:48 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,5 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
-
-        // Register the login observer
-        Event::listen(Login::class, [SendLoginPopup::class, 'handle']);
     }
 }
