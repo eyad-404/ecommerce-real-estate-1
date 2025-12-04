f20dd274e0a7d421c9c561ac534d11ccc0718cfd
pola-raouf <pola2302440@miuegypt.edu.eg>
2025-12-04 18:38:19 +0200

    Update PropertyController.php

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -16,7 +16,7 @@ class PropertyController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        
         $this->middleware(OwnsProperty::class)->only(['edit', 'update', 'destroy']);
     }
 
